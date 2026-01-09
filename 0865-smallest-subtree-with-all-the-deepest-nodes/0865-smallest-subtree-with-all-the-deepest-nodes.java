5390b226d269a0de845b38209454c398e4b06329
Dharshini Periyasamy <periyasamydharshini@gmail.com>
2026-01-09 23:59:17 +0530

    Time: 0 ms (100%), Space: 43.6 MB (33.06%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(null, 0);
+        }
+
+        Result left = dfs(root.left);
+        Result right = dfs(root.right);
+
+        if (left.depth > right.depth) {
+            return new Result(left.node, left.depth + 1);
+        } else if (right.depth > left.depth) {
+            return new Result(right.node, right.depth + 1);
+        } else {
+            return new Result(root, left.depth + 1);
+        }
+    }
+
+    static class Result {
+        TreeNode node;
+        int depth;
+
+        Result(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+}
